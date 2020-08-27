e6f56703ebf4a4f117fb86c75ff95545a82b0f77
A H <10256335+hungale@users.noreply.github.com>
2020-08-27 15:19:44 -0700

    add ColorSpan component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import "./App.css";
 import UnderlinedParagraph from "./components/UnderlinedParagraph";
+import ColoredSpan from "./components/ColoredSpan";
 
 function App() {
   return (
     <div className="App">
       <UnderlinedParagraph color="white">nothing here at the moment.</UnderlinedParagraph>
       <UnderlinedParagraph color="white">expect something soon.</UnderlinedParagraph>
+      <ColoredSpan bold color="white">
+      i'm blue, da bo dee da bo dah
+      </ColoredSpan>
     </div>
   );
 }
---
+++ src/utils/styles.js
@@ -0,0 +1,6 @@
+const COLORS = {
+  GREEN: "#6FDEA9",
+  GRAY: "#414E5A"
+};
+
+export default COLORS;
