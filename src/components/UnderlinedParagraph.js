9756fc029cc366335414dfa4d633924b6768ecf9
A H <10256335+hungale@users.noreply.github.com>
2020-08-27 15:04:11 -0700

    add documentation to UnderlinedParagraph pt2

--- src/components/UnderlinedParagraph.js
+++ src/components/UnderlinedParagraph.js
@@ -1,6 +1,10 @@
 import styled from "styled-components";
 import PropTypes from "prop-types";
 
+/*
+An underlined paragraph element. 
+Takes in a color for the font and the underline.
+*/
 const UnderlinedParagraph = styled.p`
   color: ${props => props.color};
   border-bottom: ${props => props.thickness} solid ${props => props.underlineColor};
