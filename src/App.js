c0e8cc8646bc46d9e718fbc662e91fb62bf4eea1
A H <10256335+hungale@users.noreply.github.com>
2020-08-27 16:10:00 -0700

    remove index.css and other CRA defaults, add GlobalStyle

--- src/App.js
+++ src/App.js
@@ -3,16 +3,20 @@ import "./App.css";
 import UnderlinedParagraph from "./components/UnderlinedParagraph";
 import ColoredSpan from "./components/ColoredSpan";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <UnderlinedParagraph color="white">nothing here at the moment.</UnderlinedParagraph>
-      <UnderlinedParagraph color="white">expect something soon.</UnderlinedParagraph>
+      <UnderlinedParagraph color="white">
+        nothing here at the moment.
+      </UnderlinedParagraph>
+      <UnderlinedParagraph color="white">
+        expect something soon.
+      </UnderlinedParagraph>
       <ColoredSpan bold color="white">
-      i'm blue, da bo dee da bo dah
+        i'm blue, da bo dee da bo dah
       </ColoredSpan>
     </div>
   );
-}
+};
 
 export default App;
